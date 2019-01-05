490897fc85eff96e02a63c26c7f3ec6f5c28d5bd
mrex1 <42565086+mrex1@users.noreply.github.com>
2019-01-05 16:35:59 +0800

    dots added

--- screens/components/DynamicStats.js
+++ screens/components/DynamicStats.js
@@ -1,22 +1,32 @@
 import React,{Component} from 'react'
-import {View,ScrollView,StyleSheet} from 'react-native'
+import {View,ScrollView,StyleSheet,Dimensions} from 'react-native'
 import Analysis from './Analysis'
 import Activity from './Activity'
 import Map from './Map'
+import Dots from './Dots'
 
 export default class DynamicStats extends Component{
+	state={currentPage:0}
+	
+	setCurrentPage(index){
+		this.setState({currentPage:index})
+	}
+	
 	render(){
 		return(
 			<View style={styles.container}>
-			<ScrollView style={{flex:1}} horizontal showsHorizontalScrollIndicator={false} pagingEnabled>
+			<ScrollView style={{flex:1}} horizontal showsHorizontalScrollIndicator={false} pagingEnabled onMomentumScrollEnd={(event)=>{this.setCurrentPage(event.nativeEvent.contentOffset.x/(width*0.95))}}>
 				<Activity/>
 				<Analysis/>
 				<Map/>
 			</ScrollView>
+			<Dots numofpage={3} currentPage={this.state.currentPage}/>
 			</View>
 		)
 	}
 }
+
+const {width, height} = Dimensions.get('window')
 
 const styles = StyleSheet.create({
 	container:{
