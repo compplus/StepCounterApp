39e3be5ad3e79735173d1b88a114ee3cc52fabcc
awoo424 <awoo424@gmail.com>
2019-01-07 01:41:09 +0800

    Completed UI for login screen

--- screens/Login.js
+++ screens/Login.js
@@ -1,17 +1,43 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View,TouchableWithoutFeedback,ImageBackground} from 'react-native';
-import {Images} from '../assets/assets'
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, ImageBackground,  KeyboardAvoidingView, TouchableOpacity } from 'react-native';
+import { Images } from '../assets/assets'
+import LoginForm from './LoginForm';
 
 export default class Login extends Component {
   render() {
     return (
 		<ImageBackground source={Images['background']} style={styles.container}>
-			<TouchableWithoutFeedback onPress={()=>this.props.go(1)}>
+			{/*<TouchableWithoutFeedback onPress={()=>this.props.go(1)}>
 				<Text style={styles.Name}>Login</Text>
 			</TouchableWithoutFeedback>
 			<TouchableWithoutFeedback onPress={()=>this.props.go(3)}>
 				<Text style={styles.Name}>Signup</Text>
-			</TouchableWithoutFeedback>
+			</TouchableWithoutFeedback>*/}
+
+      <KeyboardAvoidingView behavior="padding" style={styles.wrapper}>
+        <View style={styles.loginWrapper}>
+
+          <TouchableWithoutFeedback onPress={()=>this.props.go(1)}>
+            <Text style={styles.title}>Log in to your account</Text>
+          </TouchableWithoutFeedback>
+        
+          <View>
+            <LoginForm />
+          </View>
+
+          <View style={styles.signupWrapper}>
+            <Text style={styles.bottomText}>Don't have an account?  </Text>
+            <TouchableOpacity>
+              <Text style={styles.signupText}>Sign up.</Text>
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity>
+              <Text style={styles.forgotpwText}>Forgot password?</Text>
+          </TouchableOpacity>
+
+        </View>
+      </KeyboardAvoidingView>
 		</ImageBackground>
     );
   }
@@ -23,10 +49,51 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  Name: {
-    fontSize: 20,
+
+  wrapper: {
+    flex: 1
+  },
+
+  loginWrapper: {
+    flexGrow: 1,
+    justifyContent: 'center'
+  },
+
+  title: {
+    fontSize: 35,
     textAlign: 'center',
-    margin: 10,
-	color:'white'
+	  color: 'white',
+    fontWeight: '400',
+    width: 300,
+    fontFamily: 'Gill Sans'
+  },
+
+  signupWrapper: {
+    justifyContent: 'center',
+    flexDirection: "row"
+  },
+
+  bottomText: {
+    fontSize: 13,
+    textAlign: 'center',
+    color: 'white',
+    fontFamily: 'Gill Sans'
+  },
+
+  signupText: {
+    fontSize: 13,
+    textAlign: 'center',
+    color: 'white',
+    fontFamily: 'Gill Sans',
+    fontWeight: '600'
+  },
+
+  forgotpwText: {
+    fontSize: 13,
+    textAlign: 'center',
+    color: 'white',
+    fontFamily: 'Gill Sans',
+    marginTop: 10
   }
+
 });
---
+++ screens/LoginForm.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, TextInput, TouchableOpacity, TouchableHighlight } from 'react-native';
+import { Images } from '../assets/assets'
+
+export default class Login extends Component {
+  render() {
+    return (
+    <View style={styles.container}>
+
+        <TextInput 
+          placeholder="Username or email"
+          placeholderTextColor="rgba(64,64,64,0.5)"
+          returnKeyType="next"
+          onSubmitEditing={() => this.passwordInput.focus()}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+          style={styles.input}/>
+
+        <TextInput 
+          placeholder="Password"
+          placeholderTextColor="rgba(64,64,64,0.5)"
+          returnKeyType="go"
+          secureTextEntry={true}
+          style={styles.input}
+          ref={(input) => this.passwordInput = input}/>
+
+        <TouchableOpacity style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>LOGIN</Text>
+        </TouchableOpacity>
+
+    </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20
+  },
+
+  input: {
+    height: 40,
+    backgroundColor: 'rgba(255,255,255,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+    color: '#FFF',
+    paddingHorizontal: 15,
+    borderRadius: 7
+  },
+
+  buttonContainer: {
+    marginTop: 20,
+    backgroundColor: 'rgba(0,51,51,1)',
+    paddingVertical: 15,
+    borderRadius: 7
+  },
+
+  buttonText: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontWeight: '400',
+    fontFamily: 'Gill Sans'
+  }
+});
