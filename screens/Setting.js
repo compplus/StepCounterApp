948fe8ce59d7a28abc979bd267873e4051dcc64c
awoo424 <awoo424@gmail.com>
2019-02-21 05:07:15 +0800

    Updated settings page

--- screens/Setting.js
+++ screens/Setting.js
@@ -1,11 +1,87 @@
-import React,{Component} from 'react'
-import {View,StyleSheet} from 'react-native'
+import React, {Component} from 'react'
+import {View, StyleSheet, Text} from 'react-native'
+import SettingsList from 'react-native-settings-list';
+import { Image } from '../assets/assets'
 
 export default class Setting extends Component{
 	render(){
-		return(
-		<View style={styles.container}>
-		</View>)
+		var bgColor = '#DCE3F4';
+
+  	return(
+    <View style={{backgroundColor:'#EFEFF4',flex:1}}>
+      <View style={{borderBottomWidth:1, backgroundColor:'#f7f7f8',borderColor:'#c8c7cc'}}>
+        <Text style={{alignSelf:'center',marginTop:20,marginBottom:10,fontWeight:'bold',fontSize:20}}>Settings</Text>
+      </View>
+      <View style={{backgroundColor:'#EFEFF4',flex:1}}>
+        {/*<SettingsList borderColor='#c8c7cc' defaultItemSize={50}>
+          <SettingsList.Header headerStyle={{marginTop:15}}/>
+          <SettingsList.Item
+            icon={
+                <Image style={styles.imageStyle} source={require('./images/settings.png')}/>
+            }
+            hasSwitch={true}
+            switchState={this.state.switchValue}
+            switchOnValueChange={this.onValueChange}
+            hasNavArrow={false}
+            title='Airplane Mode'
+          />
+          <SettingsList.Item
+            icon={<Image style={styles.imageStyle} source={require('./images/settings.png')}/>}
+            title='Wi-Fi'
+            titleInfo='Bill Wi The Science Fi'
+            titleInfoStyle={styles.titleInfoStyle}
+            // onPress={() => Alert.alert('Route to Wifi Page')}
+          />
+          <SettingsList.Item
+            icon={<Image style={styles.imageStyle} source={require('./images/settings.png')}/>}
+            title='Blutooth'
+            titleInfo='Off'
+            titleInfoStyle={styles.titleInfoStyle}
+            // onPress={() => Alert.alert('Route to Blutooth Page')}
+          />
+          <SettingsList.Item
+            icon={<Image style={styles.imageStyle} source={require('./images/settings.png')}/>}
+            title='Cellular'
+            // onPress={() => Alert.alert('Route To Cellular Page')}
+          />
+          <SettingsList.Item
+            icon={<Image style={styles.imageStyle} source={require('./images/settings.png')}/>}
+            title='Personal Hotspot'
+            titleInfo='Off'
+            titleInfoStyle={styles.titleInfoStyle}
+            // onPress={() => Alert.alert('Route To Hotspot Page')}
+          />
+          <SettingsList.Header headerStyle={{marginTop:15}}/>
+          <SettingsList.Item
+            icon={<Image style={styles.imageStyle} source={require('./images/settings.png')}/>}
+            title='Notifications'
+            // onPress={() => Alert.alert('Route To Notifications Page')}
+          />
+          <SettingsList.Item
+            icon={<Image style={styles.imageStyle} source={require('./images/settings.png')}/>}
+            title='Control Center'
+            // onPress={() => Alert.alert('Route To Control Center Page')}
+          />
+          <SettingsList.Item
+            icon={<Image style={styles.imageStyle} source={require('./images/settings.png')}/>}
+            title='Do Not Disturb'
+            // onPress={() => Alert.alert('Route To Do Not Disturb Page')}
+          />
+          <SettingsList.Header headerStyle={{marginTop:15}}/>
+          <SettingsList.Item
+            icon={<Image style={styles.imageStyle} source={require('./images/settings.png')}/>}
+            title='General'
+            // onPress={() => Alert.alert('Route To General Page')}
+          />
+          <SettingsList.Item
+            icon={<Image style={styles.imageStyle} source={require('./images/settings.png')}/>}
+            title='Display & Brightness'
+            // onPress={() => Alert.alert('Route To Display Page')}
+          />
+        </SettingsList>*/}
+      </View>
+    </View>
+		)
 	}
 }
 
