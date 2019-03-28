bcd8a16531f393a39b17f1b436d14b6f94faed84
Jae <irrisjay@gmail.com>
2019-03-28 15:10:26 +0800

    *o test expo pedometer

--- screens/Main.js
+++ screens/Main.js
@@ -2,11 +2,17 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View, TouchableWithoutFeedback,Dimensions} from 'react-native';
 import CommonStats from './components/CommonStats'
 import DynamicStats from './components/DynamicStats'
+import { Pedometer } from "expo"
 
 export default class Main extends Component{
 	state={portrait:this.isPortrait()}
 	isPortrait(){
 		return Dimensions.get('window').height>Dimensions.get('window').width
+	}
+	componentDidMount () {
+		Pedometer.watchStepCount(result => {
+			alert (JSON .stringify (result))
+		})
 	}
 	render() {
 		return <View style={{...styles.container,flexDirection:(this.state.portrait?'column':'row')}} onLayout={()=>{this.setState({portrait:this.isPortrait()});}}>
