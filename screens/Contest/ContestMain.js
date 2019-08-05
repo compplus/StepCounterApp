d8ed1193d5f444a8b96db68909588723a2b7eb39
awoo424 <awoo424@gmail.com>
2019-08-06 03:55:55 +0800

    Edited contest page UI

--- screens/Contest/ContestMain.js
+++ screens/Contest/ContestMain.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableWithoutFeedback, FlatList, Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Image } from 'react-native';
 
 export default class ContestMain extends Component {
+    indRank = () => {
+        alert("Individual Rank!");
+    }
+
+    teamRank = () => {
+        alert("Team Rank!");
+    }
+
+    teamForm = () => {
+        alert("Team Formation!"); 
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.individualRank} />
-                <View style={styles.teamRank} />
-                <View style={styles.teamFormation} />
+                <View style={styles.individualRank}>
+                    <TouchableOpacity activeOpacity = { .5 } onPress={this.indRank}>
+                        <Image source={require('./../../assets/contest_page/individual_rank.jpg')} style={styles.imageClass}></Image>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.teamRank}>
+                    <TouchableOpacity activeOpacity = { .5 } onPress={this.teamRank}>
+                        <Image source={require('./../../assets/contest_page/team_rank.jpg')} style={styles.imageClass}></Image>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.teamFormation}>
+                    <TouchableOpacity activeOpacity = { .5 } onPress={this.teamForm}>
+                        <Image source={require('./../../assets/contest_page/team_formation.jpg')} style={styles.imageClass}></Image>
+                    </TouchableOpacity>
+                </View>
             </View>
-        )
+            )
     }
 }
 
@@ -18,23 +44,30 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#171D33',
-        alignItems: 'stretch',
-        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 
     individualRank: {
         flex: 1,
-        backgroundColor: 'yellow'
+        margin: 5,
     },
 
     teamRank: {
         flex: 1,
-        backgroundColor: 'red'
+        margin: 5,
     },
 
     teamFormation: {
         flex: 1,
-        backgroundColor: 'skyblue'
+        margin: 5,
+    },
+
+    imageClass: {
+        flex: 1,
+        height: 400,
+        width: 400,
+        borderRadius: 20,
     },
 
 });
