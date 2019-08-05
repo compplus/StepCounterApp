1a9c0cc0fafb745efe181e4018977a4c2c86d62e
awoo424 <awoo424@gmail.com>
2019-08-06 00:42:48 +0800

    Added Awards page to sidebar

--- screens/Awards.js
+++ screens/Awards.js
@@ -1,77 +1,93 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View, TouchableWithoutFeedback,FlatList, Image} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, FlatList, Image } from 'react-native';
 import AwardItem from '../components/AwardItem';
 
-let json=[
+let json = [
 
-          {title: 'Start Walking',
-          description: 'Walk 1 km',
-          imageuri: require('../assets/award_page/1km.png'),
-          status: true},
+  {
+    title: 'Start Walking',
+    description: 'Walk 1 km',
+    imageuri: require('../assets/award_page/1km.png'),
+    status: true
+  },
 
-          {title: 'Rookie Walker',
-          description: 'Walk 20 km',
-          imageuri: require('../assets/award_page/20km.png'),
-          status: true},
+  {
+    title: 'Rookie Walker',
+    description: 'Walk 20 km',
+    imageuri: require('../assets/award_page/20km.png'),
+    status: true
+  },
 
-          {title: 'Awesome Walker',
-          description: 'Walk 100 km',
-          imageuri: require('../assets/award_page/100km.png'),
-          status: true},
+  {
+    title: 'Awesome Walker',
+    description: 'Walk 100 km',
+    imageuri: require('../assets/award_page/100km.png'),
+    status: true
+  },
 
-          {title: 'Training Master',
-          description: 'Submit your record',
-          imageuri: require('../assets/award_page/weight.png'),
-          status: true},
+  {
+    title: 'Training Master',
+    description: 'Submit your record',
+    imageuri: require('../assets/award_page/weight.png'),
+    status: true
+  },
 
-          {title: 'Burn Calories',
-          description: 'burn 1000 kcal',
-          imageuri: require('../assets/award_page/burning.png'),
-          status: true},
+  {
+    title: 'Burn Calories',
+    description: 'burn 1000 kcal',
+    imageuri: require('../assets/award_page/burning.png'),
+    status: true
+  },
 
-          {title: 'Morning Walker',
-          description: 'Walk before 8am',
-          imageuri: require('../assets/award_page/earlybird.png'),
-          status: false},
+  {
+    title: 'Morning Walker',
+    description: 'Walk before 8am',
+    imageuri: require('../assets/award_page/earlybird.png'),
+    status: false
+  },
 
-          {title: 'HKU Walker',
-          description: 'Walk in HKU',
-          imageuri: require('../assets/award_page/hku.png'),
-          status: false},
+  {
+    title: 'HKU Walker',
+    description: 'Walk in HKU',
+    imageuri: require('../assets/award_page/hku.png'),
+    status: false
+  },
 
-          {
-            status: false
-          },
+  {
+    status: false
+  },
 
-          {
-            status: false
-          },
+  {
+    status: false
+  },
 
-          ]
+]
 
-export default class Awards extends Component{
+export default class Awards extends Component {
   render() {
     return (
-			<View style={styles.container}>
-				<View style={styles.awardList}>
-					<FlatList
-					style={{flex:1}}
-          numColumns= {3}
-          initialNumToRender={12}
-					data={json}
-					keyExtractor={(item, index) => index.toString()}
-					showsVerticalScrollIndicator={false}
-					renderItem={({item}) =>
-						<AwardItem
-							title={item.title}
-							description={item.description}
-							imageuri={item.imageuri}
-							status={item.status}
-						/>
-					}
-					/>
-				</View>
-			</View>
+      <View style={styles.container}>
+        <View style={styles.awardList}>
+          <FlatList
+            style={{ flex: 1 }}
+            numColumns={3}
+            initialNumToRender={12}
+            data={json}
+            keyExtractor={(item, index) => index.toString()}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) =>
+              <View style={{ flex: 2, marginTop: 20, marginBottom: 20 }}>
+                <AwardItem
+                  title={item.title}
+                  description={item.description}
+                  imageuri={item.imageuri}
+                  status={item.status}
+                />
+              </View>
+            }
+          />
+        </View>
+      </View>
     );
   }
 }
@@ -80,12 +96,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#171D33',
-	   alignItems:'center'
+    alignItems: 'center'
   },
 
-  awardList:{
-	  flex:1,
-	  width:'95%'
+  awardList: {
+    flex: 1,
+    width: '95%',
   },
 
 });
