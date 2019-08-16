53426dff897857952b5a5255d8d2b2b65161dde8
awoo424 <awoo424@gmail.com>
2019-08-16 17:14:11 +0800

    Deleted redundant components

--- components/DynamicStats.js
+++ components/DynamicStats.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
 import { View, ScrollView, StyleSheet, Dimensions } from 'react-native'
-import Analysis from './Analysis'
 import Activity from './Activity'
-import Dots from './Dots'
-import GoBack from './GoBack'
-import Map from './Map'
 
 export default class DynamicStats extends Component {
 	state = { currentPage: 0, width: width * 0.95 }
@@ -14,7 +10,6 @@ export default class DynamicStats extends Component {
 	}
 
 	render() {
-		const { currentPage, width } = this.state
 		return <View style={{ ...styles.container, width: this.props.portrait ? '95%' : undefined, height: this.props.portrait ? undefined : '95%' }} onLayout={(event) => { this.setState({ width: event.nativeEvent.layout.width }) }}>
 			<Activity />
 		</View>
